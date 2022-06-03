2709cb656704d8158b547b5b5fe6b426cd82f980
Akhil <108akhilraj@gmail.com>
2022-06-04 00:45:41 +0530

    project setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./styles/main.scss"
+import 'rsuite/dist/rsuite.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      HELLO
+    </div>
+  );
+}
+
+export default App;
