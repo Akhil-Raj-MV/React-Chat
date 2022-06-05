9e7bdd0162ff8800c73e752094e32701362b64ff
Akhil <108akhilraj@gmail.com>
2022-06-05 11:35:02 +0530

    route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react'
+import 'rsuite/dist/styles/rsuite-default.css';
 import "./styles/main.scss"
-import 'rsuite/dist/rsuite.min.css';
+import {Switch}  from 'react-router'
+import PrivateRoute from './components/PrivateRoute';
+import Home from './pages/Home';
+import PublicRoute from './components/PublicRoute';
+import SignIn from './pages/SignIn';
+
 
 function App() {
   return (
     <div className="App">
-      HELLO
+      <Switch>
+        <PublicRoute path="/signin">
+           <SignIn/>
+        </PublicRoute>
+        <PrivateRoute path="/">
+          <Home/>
+        </PrivateRoute>
+      </Switch>
     </div>
   );
 }
