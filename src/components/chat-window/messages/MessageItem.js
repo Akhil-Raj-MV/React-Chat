d3ffa829a670bf16f71ff8fec89b7ce750bdd8d6
Akhil <108akhilraj@gmail.com>
2022-06-11 01:28:45 +0530

    likes,delete and other features

--- src/components/chat-window/messages/MessageItem.js
+++ src/components/chat-window/messages/MessageItem.js
@@ -1,13 +1,22 @@
 import React, { memo } from 'react';
 import { Button } from 'rsuite';
 import { useCurrentRoom } from '../../../context/current.room.context';
+import { useHover, useMediaQuery } from '../../../misc/custom.hook';
 import { auth } from '../../../misc/firebase';
 import PresenceDot from '../../PresenceDot';
 import ProfileAvatar from '../../ProfileAvatar';
 import ProfileInfoBtnModal from './ProfileInfoBtnModal';
+import IconBtnControl from './IconBtnControl'
 
-const MessageItem = ({ message,handleAdmin }) => {
-  const { author,text } = message;
+
+const MessageItem = ({ message,handleAdmin,handleLike,handleDelete }) => {
+  const { author,text,likes,likeCount } = message;
+
+  const [selfRef,isHovered]=useHover();
+  const isMobile= useMediaQuery(('max-width:992px'));
+
+  const canShowIcons=isMobile || isHovered
+
 
   const isAdmin=useCurrentRoom(v=>v.isAdmin);
   const admins=useCurrentRoom(v=>v.admins);
@@ -18,8 +27,10 @@ const MessageItem = ({ message,handleAdmin }) => {
 
   const canGrantAdmin =isAdmin && !isAuthor;
 
+  const isLiked= likes && Object.keys(likes).includes(auth.currentUser.uid);
+
   return (
-    <li className="padded mb-1">
+    <li className={`padded mb-1 cursor-pointer ${isHovered?'bg-black-02':''}`} ref={selfRef}>
       <div className="d-flex align-items-center font-bolder mb-1">
         <PresenceDot uid={author.uid}/>
         
@@ -42,6 +53,29 @@ const MessageItem = ({ message,handleAdmin }) => {
               </Button>
           }
           </ProfileInfoBtnModal>
+
+          <IconBtnControl
+            // eslint-disable-next-line no-constant-condition
+            {...(isLiked?{color:'red'}:{})}
+            isVisible={canShowIcons}
+            iconName="heart"
+            tooltip="Like"
+            onClick={()=>handleLike(message.id)}
+            badgeContent={likeCount}
+
+            />
+
+            {isAuthor &&
+              <IconBtnControl
+              // eslint-disable-next-line no-constant-condition
+              isVisible={canShowIcons}
+              iconName="close"
+              tooltip="Delete this message"
+              onClick={()=>handleDelete(message.id)}
+              />
+            }
+
+            
 
         <span className="ml-2">{author.name}</span>
       </div>
